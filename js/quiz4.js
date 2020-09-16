cca1387468ad7dd0242ff8b5af89facc291bb0d0
Apichet Sittipon <54353591+dewapichet@users.noreply.github.com>
2020-09-16 15:37:41 +0700

    quiz4

---
+++ js/quiz4.js
@@ -0,0 +1,8 @@
+let str = 'Hello my name is Joke'
+console.log(str.length)
+
+let str1 = 'Hello my name is Joke nice to meet you!'
+console.log(str1.indexOf('Joke'))
+
+let str2 = 'Well open up your mind and see like me'
+console.log(str2.slice(5,9))
