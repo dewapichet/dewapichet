9f96c358434025854c342a946ec9e3fafb4e6000
Apichet Sittipon <54353591+dewapichet@users.noreply.github.com>
2020-09-16 14:11:32 +0700

    [Added] Fuction
    - Function
       - Add
       - Mul
       -searchCustomer
    - Arrow Function
       - addNew

---
+++ js/function.js
@@ -0,0 +1,24 @@
+function add(a,b){
+    return a+b
+}
+
+function mul(a,b){
+    return a*b
+}
+
+function searchCustomer(){
+    return{
+        name : "Prayut",
+        ...
+
+    }
+}
+
+const add = (a,b) =>  a+b
+
+const addNew = (a, b) => {
+    this.name = " Wow This.name "
+    return a+b
+} 
+
+console.log(add(2,3))
