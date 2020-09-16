235d199c6df25d841817b7609e895d6284652723
Apichet Sittipon <54353591+dewapichet@users.noreply.github.com>
2020-09-16 15:25:27 +0700

    quiz3

--- js/quiz3.js
+++ js/quiz3.js
@@ -5,5 +5,15 @@ const garde = (a) => {
     else if (a>=71 && a<=80){
         return('B')
     }
+    else if (a>=61 && a<=70){
+        return('C')
+    }
+    else if (a>=51 && a<=60){
+        return('D')
+    }
+    else if (a>=0 && a<=50){
+        return('F')
+    }
+        
 }
-console.log(garde(71))
+console.log(garde(70))
